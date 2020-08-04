0856ec3c0e7f66d925d70f3781d33676b282b013
bwapes <benwaples@gmail.com>
2020-08-03 17:16:20 -0700

    first 3 components and 3 props

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import Header from './pizza-header.js';
 import ImageHeader from './imgheader.js'
+import MainContent from './mainContent.js'
 import './App.css';
 
-const assets = [
-  '../assets/logo.jpg',
-  '../assets/fb-icon.jpg',
-  '../assets/twit-icon.jpg',
-  '../assets/gp-icon.jpg"',
-  '../assets/insta-icon.jpg',
-  '../assets/flic-icon.jpg',
-  '../assets/pint-icon (1).jpg',
-  '../assets/rss-icon.jpg',
-  '../assets/mail-icon.jpg'
-]
 
 function App() {
   return (
     <div className="App">
-      <Header src={assets.map(photo => {
-        return photo
-      })} />
+      <Header />
       <ImageHeader />
+      <MainContent />
     </div>
   );
 }
---
+++ src/mainContent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class mainContent extends Component {
+    render() {
+        return (
+            <div>
+                <p id="p-1">For the fig-swirl: Melt butter over medium head in a saucepan. Add brown sugar and stir to dissolve. Halve all of the figs and toss in the saucepan with water and lemon juice. Cook over medium heat, stirring frequently, until you have a chunky-jammy mixture. Add salt with one or two stirs, set aside and let cool completely.</p>
+                <p id="p-2">Ice cream: In a small pot over medium heat, combine milk, and granulated sugar until sugar is completely dissolved and the milk is just barely lukewarm. Whisk in the egg yolks. Set mixture in the fridge and wait until the fix mixture is cooled.</p>
+                <p id="p-3">Using an ice cream machine, pour liquids into the frozen basin and process according to manufacturer instructions, i.e., let spin and thicken for 20 minutes before adding mascarpone, fix jam mixture, and the nuts. Continue to process for +/- 10 minutes. Pour semi-frozen mixture into a pyrex dish or glass tupperware. Freeze for at least two hours before serving.</p>
+                <hr id="hr-2"></hr>
+            </div>
+        )
+    }
+}
